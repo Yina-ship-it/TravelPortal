b1f77c41facf879369fa5af152f3f957c368bc84
Yina_ship_it <najdenov7731@gmail.com>
2023-11-27 00:41:07 +0500

    Added findByName method to CountryRepository

--- src/main/java/com/example/travelportal/repositories/CountryRepository.java
+++ src/main/java/com/example/travelportal/repositories/CountryRepository.java
@@ -3,5 +3,8 @@ package com.example.travelportal.repositories;
 import com.example.travelportal.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CountryRepository extends JpaRepository<Country, Long> {
+    Optional<Country> findByName(String name);
 }
