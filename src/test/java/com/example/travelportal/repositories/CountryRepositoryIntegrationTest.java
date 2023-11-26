de80b297484c516d6e1fc57a39367146d3127f91
Yina_ship_it <najdenov7731@gmail.com>
2023-11-27 02:48:32 +0500

    created test for the method findByName in CountryRepository

---
+++ src/test/java/com/example/travelportal/repositories/CountryRepositoryIntegrationTest.java
@@ -0,0 +1,41 @@
+package com.example.travelportal.repositories;
+
+import com.example.travelportal.model.Country;
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class CountryRepositoryIntegrationTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Test
+    void findByName() {
+        // Arrange
+        Country country = Country.builder().name("CountryTest").capital("CapitalTest").build();
+        entityManager.persist(country);
+        entityManager.flush();
+
+        // Act
+        Country result = countryRepository.findByName(country.getName()).orElse(null);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(country, result);
+    }
+}
