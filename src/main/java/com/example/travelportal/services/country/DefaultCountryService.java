631905f8cfafc726f4f401938e1f950420767b0d
Yina_ship_it <najdenov7731@gmail.com>
2023-11-27 02:52:22 +0500

    implementation methods [getAllCountries, getCountryById] in DefaultCountryService

--- src/main/java/com/example/travelportal/services/country/DefaultCountryService.java
+++ src/main/java/com/example/travelportal/services/country/DefaultCountryService.java
@@ -1,19 +1,35 @@
 package com.example.travelportal.services.country;
 
 import com.example.travelportal.model.Country;
+import com.example.travelportal.repositories.CountryRepository;
 import com.example.travelportal.services.country.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Primary
 public class DefaultCountryService implements CountryService {
-    @Override
-    public List<Country> getAllCountries() {
-        return null;
+    private final CountryRepository countryRepository;
+
+    @Autowired
+    public DefaultCountryService(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
     }
 
     @Override
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Override
+    @Transactional
     public Country getCountryById(long countryId) {
-        return null;
+        return countryRepository.findById(countryId).orElse(null);
     }
 
     @Override
