dcb4ac3e48f553250d39c2cbcd0b15812e80c7a1
Yina_ship_it <najdenov7731@gmail.com>
2023-11-27 03:54:36 +0500

    created DefaultHotelService implementing HotelService

---
+++ src/main/java/com/example/travelportal/services/hotel/DefaultHotelService.java
@@ -0,0 +1,39 @@
+package com.example.travelportal.services.hotel;
+
+import com.example.travelportal.model.Hotel;
+
+import java.util.List;
+
+public class DefaultHotelService implements HotelService {
+
+
+    @Override
+    public int getHotelCountByCountryId(long countryId) {
+        return 0;
+    }
+
+    @Override
+    public List<Hotel> getAllHotels() {
+        return null;
+    }
+
+    @Override
+    public Hotel getHotelById(long hotelId) {
+        return null;
+    }
+
+    @Override
+    public Hotel updateHotel(Hotel hotel) {
+        return null;
+    }
+
+    @Override
+    public Hotel saveHotel(Hotel hotel) {
+        return null;
+    }
+
+    @Override
+    public void deleteHotelById(long hotelId) {
+
+    }
+}
