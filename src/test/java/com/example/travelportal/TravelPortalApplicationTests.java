3528e555bdc32e1d50c7b23b15a31c3d5452ed21
Yina_ship_it <najdenov7731@gmail.com>
2023-11-26 23:55:04 +0500

    init

---
+++ src/test/java/com/example/travelportal/TravelPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
