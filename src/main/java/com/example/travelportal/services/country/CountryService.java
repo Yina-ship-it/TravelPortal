1f74cfa1078ee25a8281c8fcc1a4dc4c28b765fe
Yina_ship_it <najdenov7731@gmail.com>
2023-11-27 01:15:04 +0500

    created interface CountryService and added methods

---
+++ src/main/java/com/example/travelportal/services/country/CountryService.java
@@ -0,0 +1,12 @@
+package com.example.travelportal.services.country;
+
+import com.example.travelportal.model.Country;
+
+import java.util.List;
+
+public interface CountryService {
+    List<Country> getAllCountries();
+    Country getCountryById(long countryId);
+    Country saveCountry(Country country);
+    void deleteCountryById(long countryId);
+}
