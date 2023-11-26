cfb9bcdcd4b42220f630bca0f79ed1b24122f4b2
Yina_ship_it <najdenov7731@gmail.com>
2023-11-27 01:16:46 +0500

    created DefaultCountryService implementing CountryService

---
+++ src/main/java/com/example/travelportal/services/country/DefaultCountryService.java
@@ -0,0 +1,28 @@
+package com.example.travelportal.services.country;
+
+import com.example.travelportal.model.Country;
+import com.example.travelportal.services.country.CountryService;
+
+import java.util.List;
+
+public class DefaultCountryService implements CountryService {
+    @Override
+    public List<Country> getAllCountries() {
+        return null;
+    }
+
+    @Override
+    public Country getCountryById(long countryId) {
+        return null;
+    }
+
+    @Override
+    public Country saveCountry(Country country) {
+        return null;
+    }
+
+    @Override
+    public void deleteCountryById(long countryId) {
+
+    }
+}
