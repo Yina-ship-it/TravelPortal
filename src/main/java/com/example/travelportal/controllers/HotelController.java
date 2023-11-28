b2800d4c59e80a8f5ec9f5febee6d0bc1bfdff15
Yina_ship_it <najdenov7731@gmail.com>
2023-11-28 20:51:04 +0500

    implementation method getAllHotels in HotelController

--- src/main/java/com/example/travelportal/controllers/HotelController.java
+++ src/main/java/com/example/travelportal/controllers/HotelController.java
@@ -1,28 +1,41 @@
 package com.example.travelportal.controllers;
 
 import com.example.travelportal.dto.dto.HotelDto;
+import com.example.travelportal.dto.dto.HotelDtoConverter;
 import com.example.travelportal.services.hotel.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/hotels")
 public class HotelController {
     private final HotelService hotelService;
+    private final HotelDtoConverter hotelDtoConverter;
 
     @Autowired
-    public HotelController(@Qualifier("defaultHotelService") HotelService hotelService) {
+    public HotelController(@Qualifier("defaultHotelService") HotelService hotelService,
+                           HotelDtoConverter hotelDtoConverter) {
         this.hotelService = hotelService;
+        this.hotelDtoConverter = hotelDtoConverter;
     }
 
     @GetMapping("/")
     public ResponseEntity<List<HotelDto>> getAllHotels() {
-        return null;
+        try {
+            List<HotelDto> hotelDtos = hotelService.getAllHotels().stream()
+                    .map(hotelDtoConverter::convertToDto)
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(hotelDtos, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
