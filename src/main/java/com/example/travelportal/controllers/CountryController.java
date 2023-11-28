c9754b3ea21884ef77f9ac5ea1dbfca892066d16
Yina_ship_it <najdenov7731@gmail.com>
2023-11-28 16:39:02 +0500

    declared method getAllCountries in CountryController

--- src/main/java/com/example/travelportal/controllers/CountryController.java
+++ src/main/java/com/example/travelportal/controllers/CountryController.java
@@ -1,10 +1,15 @@
 package com.example.travelportal.controllers;
 
+import com.example.travelportal.dto.country.CountryDto;
 import com.example.travelportal.services.country.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/countries")
@@ -16,6 +21,9 @@ public class CountryController {
         this.countryService = countryService;
     }
 
-
+    @GetMapping("/")
+    public ResponseEntity<List<CountryDto>> getAllCountries() {
+        return null;
+    }
 }
 
