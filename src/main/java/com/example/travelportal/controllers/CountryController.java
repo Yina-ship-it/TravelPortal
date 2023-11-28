c7ff978a7048c4ba62afb3c6fe6b9bf7a1cf9397
Yina_ship_it <najdenov7731@gmail.com>
2023-11-28 18:18:23 +0500

    declared method getCountryById in CountryController

--- src/main/java/com/example/travelportal/controllers/CountryController.java
+++ src/main/java/com/example/travelportal/controllers/CountryController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +38,11 @@ public class CountryController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CountryDto> getCountryById(@PathVariable long id) {
+        return null;
     }
 }
 
