a2b50d6a022285e6ffbbf563aca4b93caaa8c185
Yina_ship_it <najdenov7731@gmail.com>
2023-11-28 20:38:57 +0500

    declared method getAllHotels in HotelController

--- src/main/java/com/example/travelportal/controllers/HotelController.java
+++ src/main/java/com/example/travelportal/controllers/HotelController.java
@@ -1,10 +1,15 @@
 package com.example.travelportal.controllers;
 
+import com.example.travelportal.dto.dto.HotelDto;
 import com.example.travelportal.services.hotel.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/hotels")
@@ -16,5 +21,8 @@ public class HotelController {
         this.hotelService = hotelService;
     }
 
-
+    @GetMapping("/")
+    public ResponseEntity<List<HotelDto>> getAllHotels() {
+        return null;
+    }
 }
