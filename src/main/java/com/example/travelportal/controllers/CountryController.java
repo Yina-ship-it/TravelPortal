7c81a03ac98457b24bb0e08a1b85f154abbb0faf
Yina_ship_it <najdenov7731@gmail.com>
2023-11-28 18:40:43 +0500

    declared method createCountry in CountryController

--- src/main/java/com/example/travelportal/controllers/CountryController.java
+++ src/main/java/com/example/travelportal/controllers/CountryController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -53,6 +50,11 @@ public class CountryController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<CountryDto> createCountry(@RequestBody CountryDto countryDTO) {
+        return null;
     }
 }
 
