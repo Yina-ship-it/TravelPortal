fadd4d5ed24b30904431c7ee56246ab4fb66daa6
Yina_ship_it <najdenov7731@gmail.com>
2023-11-28 16:34:19 +0500

    implementation methods [convertToDto, convertToEntity] in HotelDtoConverter

--- src/main/java/com/example/travelportal/dto/dto/HotelDtoConverter.java
+++ src/main/java/com/example/travelportal/dto/dto/HotelDtoConverter.java
@@ -1,6 +1,7 @@
 package com.example.travelportal.dto.dto;
 
 import com.example.travelportal.dto.DtoConverter;
+import com.example.travelportal.model.Country;
 import com.example.travelportal.model.Hotel;
 import org.springframework.stereotype.Component;
 
@@ -8,11 +9,29 @@ import org.springframework.stereotype.Component;
 public class HotelDtoConverter implements DtoConverter<Hotel, HotelDto> {
     @Override
     public HotelDto convertToDto(Hotel hotel) {
-        return null;
+        return HotelDto.builder()
+                .id(hotel.getId())
+                .name(hotel.getName())
+                .countryId(hotel.getCountry() != null ? hotel.getCountry().getId() : null)
+                .countryName(hotel.getCountry() != null ? hotel.getCountry().getName() : null)
+                .stars(hotel.getStars())
+                .website(hotel.getWebsite())
+                .build();
     }
 
     @Override
     public Hotel convertToEntity(HotelDto hotelDto) {
-        return null;
+        Country country = Country.builder()
+                .id(hotelDto.getCountryId())
+                .name(hotelDto.getCountryName()).build();
+        return Hotel.builder()
+                .id(hotelDto.getId())
+                .name(hotelDto.getName())
+                .stars(hotelDto.getStars())
+                .country(country)
+                .website((hotelDto.getWebsite() != null && !hotelDto.getWebsite().isBlank()) ?
+                        hotelDto.getWebsite() :
+                        null)
+                .build();
     }
 }
