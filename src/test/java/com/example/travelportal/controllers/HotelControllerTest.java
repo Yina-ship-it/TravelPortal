6193d16d4e0aaf7bad9b97929b47aea9ef9b4089
Yina_ship_it <najdenov7731@gmail.com>
2023-11-28 20:50:31 +0500

    created tests in HotelControllerTest for the method getAllHotels in HotelController

---
+++ src/test/java/com/example/travelportal/controllers/HotelControllerTest.java
@@ -0,0 +1,73 @@
+package com.example.travelportal.controllers;
+
+import com.example.travelportal.dto.dto.HotelDto;
+import com.example.travelportal.dto.dto.HotelDtoConverter;
+import com.example.travelportal.model.Country;
+import com.example.travelportal.model.Hotel;
+import com.example.travelportal.repositories.CountryRepository;
+import com.example.travelportal.repositories.HotelRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@Transactional
+class HotelControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Autowired
+    private HotelDtoConverter hotelDtoConverter;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getAllHotels_ShouldReturnListOfHotels() throws Exception {
+        // Arrange
+        Country country = countryRepository.save(
+                Country.builder().name("TestCountry1").capital("TestCapital1").build());
+        List<Hotel> hotels = List.of(
+                Hotel.builder().name("TestHotel1").country(country).stars(5).build(),
+                Hotel.builder().name("TestHotel2").country(country).stars(5).build(),
+                Hotel.builder().name("TestHotel3").country(country).stars(5).build(),
+                Hotel.builder().name("TestHotel4").country(country).stars(5).build()
+        );
+        hotelRepository.saveAll(hotels);
+        List<HotelDto> hotelDtos = hotelRepository.findAll().stream()
+                .map(hotelDtoConverter::convertToDto)
+                .toList();
+
+        // Act
+        mockMvc.perform(get("/api/hotels/"))
+                // Assert
+                .andExpect(result -> System.out.println(result.getResponse().getContentAsString()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(hotelDtos)));
+    }
+}
