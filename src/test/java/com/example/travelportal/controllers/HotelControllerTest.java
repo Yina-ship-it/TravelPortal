6b0b37bd3ca1cc622e7c5a56c6bf65ee3ee5e52a
Yina_ship_it <najdenov7731@gmail.com>
2023-11-29 10:16:41 +0500

    created tests in HotelControllerTest for the method getHotelById in HotelController

--- src/test/java/com/example/travelportal/controllers/HotelControllerTest.java
+++ src/test/java/com/example/travelportal/controllers/HotelControllerTest.java
@@ -70,4 +70,34 @@ class HotelControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json(objectMapper.writeValueAsString(hotelDtos)));
     }
+
+    @Test
+    void getHotelById_WhenHotelFound_ShouldReturnOkStatusAndHotelDto() throws Exception {
+        // Arrange
+        Country country = countryRepository.save(
+                Country.builder().name("TestCountry1").capital("TestCapital1").build());
+        Hotel hotel = hotelRepository.save(
+                Hotel.builder().name("TestHotel1").country(country).stars(5).build());
+        HotelDto hotelDto = hotelDtoConverter.convertToDto(hotel);
+
+        // Act
+        mockMvc.perform(get("/api/hotels/{id}", hotel.getId()))
+                // Assert
+                .andExpect(result -> System.out.println(result.getResponse().getContentAsString()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(hotelDto)));
+    }
+
+    @Test
+    void getHotelById_WhenHotelNotFound_ShouldReturnNotFoundStatus() throws Exception {
+        // Arrange
+        long id = Long.MAX_VALUE;
+
+        // Act
+        mockMvc.perform(get("/api/hotels/{id}", id))
+                // Assert
+                .andExpect(result -> System.out.println(result.getResponse().getContentAsString()))
+                .andExpect(status().isNotFound());
+    }
 }
