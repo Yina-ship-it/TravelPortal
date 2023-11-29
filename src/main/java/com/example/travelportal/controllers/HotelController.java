1414b694e40ef4cd21b36d522b5892eb2b686aa9
Yina_ship_it <najdenov7731@gmail.com>
2023-11-29 10:04:20 +0500

    declared method getHotelById in HotelController

--- src/main/java/com/example/travelportal/controllers/HotelController.java
+++ src/main/java/com/example/travelportal/controllers/HotelController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +38,10 @@ public class HotelController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HotelDto> getHotelById (@PathVariable long id) {
+        return null;
     }
 }
