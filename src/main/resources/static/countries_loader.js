970803b4ed41e2a0a510210193b7712949bd2a50
Yina_ship_it <najdenov7731@gmail.com>
2023-12-04 13:37:11 +0500

    created countries_loader.js and added functions loadCountries and displayCountries

---
+++ src/main/resources/static/countries_loader.js
@@ -0,0 +1,38 @@
+function loadCountries() {
+    fetch('/api/countries/')
+        .then(response => response.json())
+        .then(data => {
+            displayCountries(data);
+        })
+        .catch(error => console.error('Ошибка при загрузке стран:', error));
+}
+
+function displayCountries(countries) {
+    const tableBody = document.querySelector('#countriesTable tbody');
+    tableBody.innerHTML = '';
+
+    countries.forEach(country => {
+        const row = document.createElement('tr');
+
+        const nameCell = document.createElement('td');
+        nameCell.textContent = country.name;
+        row.appendChild(nameCell);
+
+        const capitalCell = document.createElement('td');
+        capitalCell.textContent = country.capital;
+        row.appendChild(capitalCell);
+
+        const actionsCell = document.createElement('td');
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Редактировать';
+        actionsCell.appendChild(editButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Удалить';
+        actionsCell.appendChild(deleteButton);
+
+        row.appendChild(actionsCell);
+
+        tableBody.appendChild(row);
+    });
+}
