43baecfba1829fb5a45098d5cdd30c7ef24e5dd3
Yina_ship_it <najdenov7731@gmail.com>
2023-12-04 15:46:05 +0500

    Added findAllByCountry_IdAndNameFragmentOrderByStarsDesc method to HotelRepository

--- src/main/java/com/example/travelportal/repositories/HotelRepository.java
+++ src/main/java/com/example/travelportal/repositories/HotelRepository.java
@@ -2,10 +2,20 @@ package com.example.travelportal.repositories;
 
 import com.example.travelportal.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
     Optional<Hotel> findByCountry_IdAndName(long countryId, String hotelName);
     int countByCountry_Id(long countryId);
+
+    @Query("""
+            SELECT h FROM Hotel h
+            WHERE h.country.id = :countryId
+                AND LOWER(h.name) LIKE LOWER(CONCAT('%', :fragment, '%'))
+            ORDER BY h.stars DESC, h.name ASC
+            """)
+    List<Hotel> findAllByCountry_IdAndNameFragmentOrderByStarsDesc(long countryId, String fragment);
 }
