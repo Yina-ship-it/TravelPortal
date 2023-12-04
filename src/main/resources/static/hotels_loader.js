4f3ec6bbca0949520b506eade58d4a62d0d765a2
Yina_ship_it <najdenov7731@gmail.com>
2023-12-04 14:46:21 +0500

    added function editHotel for editButton in hotels_loader.js

--- src/main/resources/static/hotels_loader.js
+++ src/main/resources/static/hotels_loader.js
@@ -49,6 +49,7 @@ function displayHotels(hotels) {
         const actionsCell = document.createElement('td');
         const editButton = document.createElement('button');
         editButton.textContent = 'Редактировать';
+        editButton.onclick = () => editHotel(hotel.id, nameCell, countryCell, starsCell, websiteCell, actionsCell);
         actionsCell.appendChild(editButton);
 
         const deleteButton = document.createElement('button');
@@ -75,4 +76,90 @@ function deleteHotel(hotelId, hotelName) {
             }
         })
         .catch(error => console.error(`Ошибка при удалении отеля ${hotelId}:`, error));
+}
+
+function editHotel(hotelId, nameCell, countryCell, starsCell, websiteCell, actionsCell) {
+    const currentName = nameCell.textContent;
+    const currentCountryName = countryCell.textContent;
+    const currentStars = starsCell.textContent;
+    const currentWebsite = websiteCell.textContent;
+
+    const nameInput = document.createElement('input');
+    nameCell.innerHTML = '';
+    nameCell.appendChild(nameInput);
+    nameInput.value = currentName;
+
+    const countryInput = document.createElement('select');
+    fetch('/api/countries/')
+        .then(response => response.json())
+        .then(countries => {
+            countries.forEach(country => {
+                const option = document.createElement('option');
+                option.value = country.id;
+                option.text = country.name;
+                if (country.name === currentCountryName) {
+                    option.selected = true
+                    countryInput.value = country.id;
+                }
+                countryInput.appendChild(option);
+            });
+        })
+        .catch(error => console.error('Error loading countries:', error));
+    countryCell.innerHTML = '';
+    countryCell.appendChild(countryInput);
+
+    const starsInput = document.createElement('input');
+    starsInput.setAttribute('type', 'number');
+    starsInput.setAttribute('min', '1');
+    starsInput.setAttribute('max', '5');
+    starsCell.innerHTML = '';
+    starsCell.appendChild(starsInput);
+    starsInput.value = currentStars;
+
+    const websiteInput = document.createElement('input');
+    websiteInput.setAttribute('type', 'url');
+    websiteCell.innerHTML = '';
+    websiteCell.appendChild(websiteInput);
+    websiteInput.value = currentWebsite;
+
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Сохранить';
+    saveButton.onclick = () => {
+        fetch(`/api/hotels/${hotelId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: nameInput.value,
+                countryId: countryInput.value,
+                stars: starsInput.value,
+                website: websiteInput.value
+            }),
+        })
+            .then(response => {
+                if (response.ok) {
+                    alert(`Отель "${nameInput.value}" успешно отредактирована.`);
+                    loadHotels();
+                } else {
+                    alert(`Не удалось отредактировать отель "${nameInput.value}". Возможно, это невозможно на данный момент.`);
+                }
+            })
+            .then(updatedHotel => {
+                nameCell.textContent = updatedHotel.name;
+                countryCell.textContent = updatedHotel.countryName;
+                starsCell.textContent = updatedHotel.stars;
+                websiteCell.textContent = updatedHotel.website;
+            })
+            .catch(error => console.error('Ошибка при сохранении отеля:', error));
+    };
+
+    const cancelButton = document.createElement('button');
+    cancelButton.textContent = 'Отмена';
+    cancelButton.type = 'button';
+    cancelButton.onclick = () => loadHotels();
+
+    actionsCell.childNodes.forEach(action => action.style.display = 'none')
+    actionsCell.appendChild(saveButton);
+    actionsCell.appendChild(cancelButton);
 }
