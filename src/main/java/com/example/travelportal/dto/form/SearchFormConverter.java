9a68af1c174360274dc282aea4b89e4091b49af9
Yina_ship_it <najdenov7731@gmail.com>
2023-12-04 17:09:13 +0500

    implementation methods [convertToDto, convertToEntity] in SearchFormConverter

--- src/main/java/com/example/travelportal/dto/form/SearchFormConverter.java
+++ src/main/java/com/example/travelportal/dto/form/SearchFormConverter.java
@@ -2,15 +2,34 @@ package com.example.travelportal.dto.form;
 
 import com.example.travelportal.dto.DtoConverter;
 import com.example.travelportal.model.Country;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SearchFormConverter implements DtoConverter<Country, SearchForm> {
     @Override
     public SearchForm convertToDto(Country country) {
-        return null;
+        return new SearchForm("", country.toString());
     }
 
     @Override
     public Country convertToEntity(SearchForm searchForm) {
-        return null;
+        try {
+            String selectedCountry = searchForm.getSelectedCountry();
+
+            long id = Long.parseLong((selectedCountry.split(", ")[0].split("=")[1]));
+            String name = selectedCountry.split(", ")[1].split("=")[1];
+            String capital = selectedCountry.split(", ")[2].split("=")[1].replace(")", "");
+
+            return new Country(id,
+                    getNonBlankString(name),
+                    getNonBlankString(capital));
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
+
+    private String getNonBlankString(String value) {
+        return (value != null && !value.isBlank()) ? value : null;
+    }
+
 }
