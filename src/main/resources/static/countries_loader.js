73c45bc6873531065b1cc5f3fe319104d09d91cf
Yina_ship_it <najdenov7731@gmail.com>
2023-12-04 13:56:21 +0500

    added function editCountry for editButton in countries_loader.js

--- src/main/resources/static/countries_loader.js
+++ src/main/resources/static/countries_loader.js
@@ -40,6 +40,7 @@ function displayCountries(countries) {
         const actionsCell = document.createElement('td');
         const editButton = document.createElement('button');
         editButton.textContent = 'Редактировать';
+        editButton.onclick = () => editCountry(country.id, nameCell, capitalCell, actionsCell);
         actionsCell.appendChild(editButton);
 
         const deleteButton = document.createElement('button');
@@ -67,4 +68,54 @@ function deleteCountry(countryId, countryName) {
             }
         })
         .catch(error => console.error(`Ошибка при удалении страны ${countryId}:`, error));
+}
+
+function editCountry(countryId, nameCell, capitalCell, actionsCell) {
+    const currentName = nameCell.textContent;
+    const currentCapital = capitalCell.textContent;
+
+    const nameInput = document.createElement('input');
+    nameCell.innerHTML = '';
+    nameCell.appendChild(nameInput);
+    nameInput.value = currentName;
+    const capitalInput = document.createElement('input');
+    capitalInput.value = currentCapital;
+    capitalCell.innerHTML = '';
+    capitalCell.appendChild(capitalInput)
+
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Сохранить';
+    saveButton.onclick = function() {
+        fetch(`/api/countries/${countryId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: nameInput.value,
+                capital: capitalInput.value,
+            }),
+        })
+            .then(response => {
+                if (response.ok) {
+                    alert(`Страна "${nameInput.value}" успешно отредактирована.`);
+                    loadCountries();
+                } else {
+                    alert(`Не удалось сохранить страну "${nameInput.value}". Возможно, это невозможно на данный момент.`);
+                }
+            })
+            .catch(error => console.error('Ошибка при редактировании страны:', error));
+    };
+
+    const cancelButton = document.createElement('button');
+    cancelButton.textContent = 'Отмена';
+    cancelButton.onclick = function() {
+        loadCountries()
+    };
+    actionsCell.childNodes.forEach(action =>{
+        action.style.display = 'none'
+    })
+    actionsCell.appendChild(saveButton)
+    actionsCell.appendChild(cancelButton)
+    console.log(actionsCell.childElementCount)
 }
