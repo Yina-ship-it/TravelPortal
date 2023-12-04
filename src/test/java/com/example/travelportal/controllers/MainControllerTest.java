6d98b047f38a0562e25c145978a7ae40baac9de3
Yina_ship_it <najdenov7731@gmail.com>
2023-12-04 17:40:59 +0500

    created tests in MainControllerTest for search in MainController

--- src/test/java/com/example/travelportal/controllers/MainControllerTest.java
+++ src/test/java/com/example/travelportal/controllers/MainControllerTest.java
@@ -1,5 +1,10 @@
 package com.example.travelportal.controllers;
 
+import com.example.travelportal.model.Country;
+import com.example.travelportal.model.Hotel;
+import com.example.travelportal.repositories.CountryRepository;
+import com.example.travelportal.repositories.HotelRepository;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +16,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -26,6 +34,12 @@ class MainControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @Autowired
+    private CountryRepository countryRepository;
+
     @Test
     void testHome() throws Exception {
         mockMvc.perform(get("/"))
@@ -33,5 +47,26 @@ class MainControllerTest {
                 .andExpect(view().name("index"))
                 .andExpect(model().attributeExists("searchForm"))
                 .andExpect(model().attributeExists("countries"));
+    }
+
+    @Test
+    void testSearch() throws Exception {
+        Country country = countryRepository.save(
+                Country.builder().name("TestCountry").capital("TestCapital").build());
+        List<Hotel> hotels = new ArrayList<>(List.of(
+                Hotel.builder().name("HotelTest1").country(country).stars(5).build(),
+                Hotel.builder().name("HotelTest2").country(country).stars(4).build(),
+                Hotel.builder().name("HotelTest3").country(country).stars(3).build(),
+                Hotel.builder().name("HotelTest4").country(country).stars(2).build(),
+                Hotel.builder().name("HotelTest5").country(country).stars(1).build()
+        ));
+        hotelRepository.saveAll(hotels);
+
+        mockMvc.perform(get("/search")
+                        .param("searchInput", "eltte")
+                        .param("selectedCountry", country.toString()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("searchResults"))
+                .andExpect(model().attributeExists("searchResults"));
     }
 }
